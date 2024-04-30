8d4c04a17aace8d7c7607f7c3421f35fb4b61bd7
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-30 19:16:15 +0300

    function declaration practice

---
+++ functions.js
@@ -0,0 +1,15 @@
+// types of functions
+
+// function declaration
+
+function ralphTonight() {
+    const electricity = false;
+    if (electricity){
+        console.log("I will code tonight");
+    }else{
+        console.log("More sleep for me I guess");
+
+    }
+};
+
+ralphTonight();
